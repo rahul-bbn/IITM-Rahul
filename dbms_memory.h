11060232cc9cc2f2e75aa45e8566e7bd41f3ca0f
Rahul <bansalgippcy@gmail.com>
2025-08-08 10:48:25 +0530

    whole file is replcaed

---
+++ dbms_memory.h
@@ -0,0 +1,6 @@
+#ifndef DBMS_MEMORY_H
+#define DBMS_MEMORY_H
+
+void run_dbms_slab_test();
+
+#endif // DBMS_MEMORY_H
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "dbms_memory.h"
+#include <stdio.h>
+
+int main() {
+    printf("DBMS Slab Memory System Test\n");
+    run_dbms_slab_test();
+    return 0;
+}
