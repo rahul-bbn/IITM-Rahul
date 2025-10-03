1ae14da08faf6431b1c521ff5463bb4c1b24dedb
Rahul <bansalgippcy@gmail.com>
2025-10-03 09:47:53 +0530

    added code

--- MSG_QUEUE/MSG_QUEUE_TASK/sender.c
+++ MSG_QUEUE/MSG_QUEUE_TASK/sender.c
@@ -1,56 +1,42 @@
-//sender_chat.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <unistd.h>
-#include <signal.h>
-
-#define MSG_KEY 1234
-
-struct msg_buffer {
-    long msg_type;
-    char sender[20];
-    char text[100];
-};
-
-int msgid;
-struct msg_buffer message;
-
-void handle_exit(int sig) {
-    strcpy(message.text, "__EXIT__");
-    msgsnd(msgid, &message, sizeof(message) - sizeof(long), 0);
-    printf("\n[System]: Exiting chat...\n");
-    exit(0);
-}
+#include "msg_common.h"
 
 int main() {
-    msgid = msgget(MSG_KEY, 0666 | IPC_CREAT);
-    if (msgid == -1) {
+    key_t key;
+    int msgid;
+    struct msg_buffer msg;
+
+    key = ftok("chatfile", 65);
+
+    msgid = msgget(key, 0666 | IPC_CREAT);
+    if (msgid < 0) {
         perror("msgget");
         exit(1);
     }
 
-    message.msg_type = 1;
+    printf("User1 ready. Type messages (type 'exit' to quit):\n");
 
-    printf("Enter your name: ");
-    fgets(message.sender, sizeof(message.sender), stdin);
-    message.sender[strcspn(message.sender, "\n")] = '\0';
+    if (fork() == 0) {
 
-    // Catch Ctrl+C
-    signal(SIGINT, handle_exit);
-
-    printf("Type messages (Ctrl+C to quit):\n");
-    while (1) {
-        if (!fgets(message.text, sizeof(message.text), stdin)) {
-            handle_exit(0); // if input stream closed (e.g., terminal closed)
+        while (1) {
+            msgrcv(msgid, &msg, sizeof(msg.mtext), 2, 0); 
+            if (strncmp(msg.mtext, "exit", 4) == 0) {
+                printf("User2 exited. Closing chat.\n");
+                break;
+            }
+            printf("User2: %s\n", msg.mtext);
         }
-        message.text[strcspn(message.text, "\n")] = '\0';
+        exit(0);
+    } else {
+        
+        while (1) {
+            fgets(msg.mtext, MAX_SIZE, stdin);
+            msg.mtext[strcspn(msg.mtext, "\n")] = '\0'; 
+            msg.mtype = 1; 
+            msgsnd(msgid, &msg, sizeof(msg.mtext), 0);
 
-        if (msgsnd(msgid, &message, sizeof(message) - sizeof(long), 0) == -1) {
-            perror("msgsnd");
-            exit(1);
+            if (strncmp(msg.mtext, "exit", 4) == 0) {
+                break;
+            }
         }
     }
 
